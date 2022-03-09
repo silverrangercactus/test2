b170f9331686d98f0f189ae6a9bab55823404774
silverrangercactus <silverrangercactus@gmail.com>
2022-03-09 20:58:54 +0500

    squash commit My

---
+++ test2/test2/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by Fedor Boriskin on 09.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var ololo = 4
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("1")
+        print("2")
+        print("3")
+        print(ololo)
+    }
+    
+    
+}
+
